ea26f237c6ea106a065c0b66b03db7ce64d75b93
Reece Pohlman <reecepohlman@gmail.com>
2023-08-29 16:41:25 -0400

    updated to vite

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,7 +1,5 @@
-import React from 'react';
-import logo from './logo.svg';
 import { useState, useEffect } from 'react';
-import './App.css';
+import './styles.css';
 import axios from 'axios';
 import { Header, List } from 'semantic-ui-react';
 
